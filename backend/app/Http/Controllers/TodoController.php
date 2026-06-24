05e918fe9894e75878db3099961b914ed212a65d
Himomori <Himomori@bk.ru>
2026-06-24 23:58:00 +1100

    Добавлены модель Todo и контроллер с CRUD-методами

---
+++ backend/app/Http/Controllers/TodoController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class TodoController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $todos = Todo::orderBy('created_at', 'desc')->get();
+        return response()->json($todos);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $todo = Todo::create([
+            'title' => $request->title,
+            'completed' => false,
+        ]);
+
+        return response()->json($todo, 201);
+    }
+
+    public function update(Request $request, Todo $todo): JsonResponse
+    {
+        $request->validate([
+            'title'     => 'sometimes|string|max:255',
+            'completed' => 'sometimes|boolean',
+        ]);
+
+        $todo->update($request->only(['title', 'completed']));
+
+        return response()->json($todo);
+    }
+
+    public function destroy(Todo $todo): JsonResponse
+    {
+        $todo->delete();
+
+        return response()->json(null, 204);
+    }
+}
