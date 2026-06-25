f1f3c625919c00008ab66d0873e0489efe249775
Himomori <Himomori@bk.ru>
2026-06-25 23:56:26 +1100

    Убран лишний префикс api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/api/todos', TodoController::class);
+Route::apiResource('/todos', TodoController::class);
