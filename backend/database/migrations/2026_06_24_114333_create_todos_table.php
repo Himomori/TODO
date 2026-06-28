5c9c38cf1319626e45846648d0b00715f856b329
Himomori <Himomori@bk.ru>
2026-06-28 12:10:58 +1100

    Добавления поля в миграции

--- backend/database/migrations/2026_06_24_114333_create_todos_table.php
+++ backend/database/migrations/2026_06_24_114333_create_todos_table.php
@@ -6,22 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
 {
     Schema::create('todos', function (Blueprint $table) {
         $table->id();
         $table->string('title');
         $table->boolean('completed')->default(false);
+        $table->enum('priority', ['high', 'medium', 'low'])->default('medium');
         $table->timestamps();
     });
 }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('todos');
